976d4c74d3d1e909a5dce1a8cf9dba9e242c6fa8
Uyen <uyenntt17407@st.uel.edu.vn>
2022-09-16 21:31:04 +0700

    table

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -4,33 +4,40 @@ import DropdownComponent from "./DropdownComponent";
 import { useState } from "react";
 import { listUsers } from "./data";
 import InputComponent from "./InputComponent";
+import TableComponent from "./TableComponent";
 
-const listOptSearch = [
-  {
-    name: "User Name",
-    key: "username",
-  },
-  {
-    name: "Email",
-    key: "email",
-  },
-  {
-    name: "Age",
-    key: "age",
-  },
-];
+// const listOptSearch = [
+//   {
+//     name: "User Name",
+//     key: "username",
+//   },
+//   {
+//     name: "Email",
+//     key: "email",
+//   },
+//   {
+//     name: "Age",
+//     key: "age",
+//   },
+// ];
 
 function App() {
-  const [users, setUsers] = useState([...listUsers]);
+
+
+
+  // const [users, setUsers] = useState([...listUsers]);
   return (
     <div className='App'>
-      {/*component input search */}
-      <InputComponent/>
-      <DropdownComponent title={"Selection search"} optSearch={listOptSearch} />
+      {/* component input search */}
+      {/* <InputComponent/>
+      <DropdownComponent title={"Selection search"} optSearch={listOptSearch} /> */}
 
       {/*component dropdown list */}
 
-      {/*component table use */}
+      <TableComponent listUsers={listUsers} />
+      {
+
+      }
 
       {/*component pagination */}
     </div>
--- exercise/src/TableComponent.jsx
+++ exercise/src/TableComponent.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
 
-export default function TableComponent() {
+export default function TableComponent({ listUsers }) {
+  console.log(listUsers)
   return (
-    <div>TableComponent</div>
+    <div className='container'>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers.map(({ id, first_name, last_name, email, gender, age, salary }, index) => (
+            <tr>
+              <td>{id}</td>
+              <td>{first_name}</td>
+              <td>{last_name}</td>
+              <td>{email}</td>
+              <td>{gender}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   )
 }
