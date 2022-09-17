0f7cc45225a86e585cae1010f7e9f0f47fd08941
TonyTran230898 <75846083+tonytran230898@users.noreply.github.com>
2022-09-17 22:07:42 +0700

    testlist

--- spotyfy-app/src/AppSpotifyGroupTwo.jsx
+++ spotyfy-app/src/AppSpotifyGroupTwo.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import instance from "./apiConfig";
+import ListArtists from "./components/ListArtists";
 
 export default function AppSpotifyGroupTwo() {
   const [artistsSearch, setArtistsSearch] = useState([]);
   const [tracks, setTracks] = useState([]);
   //artistsSearch: lưu dữ liệu artis khi search => list artis result
 
-  return (
-    <div>
-      {/*Input component: search ấn enter  => call api*/}
-      {/*List result artis */}
-      {/* click item in arties => tracks component*/}
-    </div>
-  );
+  return <ListArtists />;
 }
